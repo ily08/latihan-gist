d08bb3fda8bc85b9ddd9f265810735d2f4a73278
ily08 <ilyas_206@smkassalaambandung.sch.id>
2022-07-26 15:17:36 +0700

    gist

---
+++ latihan1/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <fieldset>
+        <center>
+            <form action="soal1.php" method="post">
+                <table>
+                    <tr>
+                        <td>Nomor</td>
+                        <td><input type="number" name="no" id=""></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><input type="submit" name="save" id=" "></td>
+                    </tr>
+                </table>
+            </form>
+        </center>
+    </fieldset>
+</body>
+</html>
---
+++ latihan1/soal1.php
@@ -0,0 +1,8 @@
+ <?php
+    if(isset($_POST['save']))
+    {
+        $no = $_POST['nomor'];
+        
+        echo "No Anda : $no";
+    }
+ ?>
